99a1bbf6f990caef1e066516ed27e82f3a618f89
Ritwik Singh Yadav <singhritwik9@gmail.com>
2023-02-16 13:06:00 +0530

    skip button changes

--- src/components/slider/ControlBtn.js
+++ src/components/slider/ControlBtn.js
@@ -2,13 +2,11 @@ export function NextButton({ onClickHandler, condition, label }) {
   return (
     <div className="pt-[26px]">
       <button
-        className={` ${
-          true ? "bg-[#F19B6C]" : "bg-[#3E3E3E]"
-        } py-[12px] px-[24px] h-[41px]w-[118px] max-w-[118px] sm:w-[118pxpx] sm:max-w-[118pxpx] rounded-full ${
-          condition == true
+        className={` ${true ? "bg-[#F19B6C]" : "bg-[#3E3E3E]"
+          } py-[12px] px-[24px] h-[41px]w-[118px] max-w-[118px] sm:w-[118pxpx] sm:max-w-[118pxpx] rounded-full ${condition == true
             ? "hover:bg-[#ee854d] "
             : "disabled:hover:bg-[#ee854d] cursor-no-drop "
-        } text-black gap-1 font-semibold font-Inter tracking-[0.42px] text-[16px]`}
+          } text-black gap-1 font-semibold font-Inter tracking-[0.42px] text-[16px]`}
         onClick={onClickHandler}
       >
         {label}
@@ -22,11 +20,8 @@ export function SkipButton({ onClickHandler, condition, label }) {
     <div className="pt-[26px]">
       <button
         className={` "bg-[transparent]" text-white
-         py-[12px] px-[24px] h-[41px]w-[118px] max-w-[118px] sm:w-[118px] sm:max-w-[118pxpx] rounded-full ${
-           condition == true
-             ? "hover:bg-[#ee854d] "
-             : "disabled:hover:bg-[#ee854d] cursor-no-drop "
-         } text-black gap-1 font-semibold font-Inter tracking-[0.42px] text-[16px]`}
+         py-[12px] px-[24px] h-[41px]w-[118px] max-w-[118px] sm:w-[118px] sm:max-w-[118pxpx] rounded-full
+         gap-1 font-semibold font-Inter tracking-[0.42px] text-[16px]`}
         onClick={onClickHandler}
       >
         {label}
