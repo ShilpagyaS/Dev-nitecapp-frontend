c28e66af0a92457a8b8cc79e26a5f01420fda36e
Nanak Seth <millionairemindset144@gmail.com>
2023-03-06 20:46:11 +0530

    barChart

--- src/components/userDashboard-comp/SalesSection.js
+++ src/components/userDashboard-comp/SalesSection.js
@@ -7,10 +7,10 @@ function SalesSection() {
             <h5 className='not-italic font-semibold text-2xl font-Inter leading-tight text-white mb-[33px]'>
                 Sales
             </h5>
-            <div className='w-full h-[263px] border border-[#3C3C3C] rounded-[13px] ' >
+              <div>
                 <ChartVerticalBar />
-            </div>
-
+              </div>
+ 
         </div>
     )
 }
--- src/utils/ChartVerticalBar.js
+++ src/utils/ChartVerticalBar.js
@@ -1,56 +1,43 @@
-import React from 'react';
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend,
-} from 'chart.js';
-import { Bar } from 'react-chartjs-2';
-
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
-);
-
-export const options = {
-    responsive: true,
-    plugins: {
-        legend: {
-            position: 'top',
-        },
-        title: {
-            display: true,
-            text: 'Chart.js Bar Chart',
-        },
-    },
-};
-
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-export const data = {
-    labels,
-    datasets: [
-        {
-            label: 'Your Sale',
-            data: [65, 59, 80, 81, 56, 55, 40],
-            backgroundColor: '#f19b6c',
-        },
-        {
-            label: 'Others Sale',
-            data: [6, 49, 90, 32, 59, 55, 70],
-            backgroundColor: 'rgba(255, 255, 255, 0.2)',
-        },
-
-    ],
-};
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import "swiper/css";
+import coctailMock from ".././components/mock/CoctailMock";
+import {BarChart} from "./barChart"
 export function ChartVerticalBar() {
-    return <Bar options={{ maintainAspectRatio: false } } data={data} />;
+    {
+        const coctailData = coctailMock.coctailData;
+
+        return (
+            <div style={{backgroundColor:'#2C2C2C' }} className=" mt-5 w-full">
+                <Swiper
+                    slidesPerView={"auto"}
+                    spaceBetween={20}
+                    modules={[Navigation, Pagination, A11y]}
+                    navigation
+                      pagination={{ clickable: true }}
+                      scrollbar={{ draggable: true }}
+            
+                >
+                    {coctailData.map((slide, i) => {
+                        return (
+                            <SwiperSlide
+                                key={i}
+                                style={{
+                                    width: "fit-content",
+                                    padding: "15px 0px",
+                                    display: "flex",
+                                }}
+                                className="mb-8 w-[15px]"
+                            >
+                              <div style={{ backgroundColor:'#2C2C2C' }}>
+                              <BarChart/>
+                                </div>  
+                            </SwiperSlide>
+                        );
+                    })}
+                </Swiper>
+              
+            </div>
+        );
+    }
 }
