43bba1426f23083d03e3fc39fe02956e8081883c
Ritwik Singh Yadav <singhritwik9@gmail.com>
2023-03-07 13:13:51 +0530

    fixed learning graph

--- src/utils/ChartVerticalBar.js
+++ src/utils/ChartVerticalBar.js
@@ -2,21 +2,21 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import "swiper/css";
 import coctailMock from ".././components/mock/CoctailMock";
-import {BarChart} from "./barChart"
+import { BarChart } from "./barChart"
 export function ChartVerticalBar() {
     {
         const coctailData = coctailMock.coctailData;
 
         return (
-            <div style={{backgroundColor:'#2C2C2C' }} className=" mt-5 w-full">
+            <div style={{ backgroundColor: '#2C2C2C' }} className=" mt-5 w-full">
                 <Swiper
                     slidesPerView={"auto"}
                     spaceBetween={20}
                     modules={[Navigation, Pagination, A11y]}
                     navigation
-                      pagination={{ clickable: true }}
-                      scrollbar={{ draggable: true }}
-            
+                    pagination={{ clickable: true }}
+                    scrollbar={{ draggable: true }}
+
                 >
                     {coctailData.map((slide, i) => {
                         return (
@@ -29,14 +29,17 @@ export function ChartVerticalBar() {
                                 }}
                                 className="mb-8 w-[15px]"
                             >
-                              <div style={{ backgroundColor:'#2C2C2C' }}>
-                              <BarChart/>
-                                </div>  
+                                <div className="pt-[11px] bg-[#2C2C2C] rounded-[12px]"> 
+                                <p className="not-italic font-semibold text-base font-Inter text-white bg-[#2C2C2C] rounded-[12px] pl-[11px]"> Total Sales  |  This Week</p>
+
+                                    <BarChart />
+                                </div>
+                             
                             </SwiperSlide>
                         );
                     })}
                 </Swiper>
-              
+
             </div>
         );
     }
--- src/utils/barChart.js
+++ src/utils/barChart.js
@@ -19,35 +19,43 @@ ChartJS.register(
     Legend
 );
 
-export const options = {   
-     
+const plugin = {
+    id: 'customCanvasBackgroundColor',
+    beforeDraw: (chart, args, options) => {
+        const { ctx } = chart;
+        ctx.save();
+        ctx.globalCompositeOperation = 'destination-over';
+        ctx.fillStyle = options.color || '#2C2C2C';
+        ctx.fillRect(0, 0, chart.width, chart.height);
+        ctx.restore();
+    }
+};
+export const options = {
+
     scales: {
         y: {
             ticks: {
                 // Include a dollar sign in the ticks
-                callback: function(value) {
+                callback: function (value) {
                     return '$' + value;
                 },
-                
+
             }
         }
     },
     maintainAspectRatio: false,
     responsive: true,
     plugins: {
-        
-          legend: {
+
+        legend: {
             display: false
-           },
-           backgroundColor: {
-            color: 'red',
-          },
-      
-        title: {
-            display: true,
-            text: 'Total Sales  |  This Week ',
         },
+        customCanvasBackgroundColor: {
+            color: '#2C2C2C',
+        }
+
     },
+
 };
 
 const labels = ['M', 'T', 'W', 'T', 'F', 'S', 'S'];
@@ -55,7 +63,7 @@ const labels = ['M', 'T', 'W', 'T', 'F', 'S', 'S'];
 export const data = {
     labels,
     datasets: [
-     
+
         {
             label: 'Your Sale',
             data: [6, 49, 90, 32, 59, 55, 70],
@@ -67,19 +75,15 @@ export const data = {
 };
 
 export function BarChart() {
-    return ( 
-            <div style={{backgroundColor:'#2C2C2C'}}>
-             <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 ">
-              <div className="bg-white rounded-lg shadow-lg">
-                 <div className="">
-                   <div className="w-full">
-                     <div  style={{ height: "306px", width:'545px' }}>
-                      <Bar options={options} data={data} />
-                     </div>
-                   </div>
-               </div>
-              </div>
-             </div>
-           </div>
-      );
+    return (
+
+
+
+        <div className="h-[306px] w-[545px] bg-[#2C2C2C] rounded-[12px] p-[11px]">
+            <Bar options={options} plugins={[plugin]} data={data} />
+
+        </div>
+
+
+    );
 }
