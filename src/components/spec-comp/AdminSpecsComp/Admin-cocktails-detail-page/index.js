91d7f1b8dfc57068e285d18263e567bd0329735d
Ritwik Singh Yadav <singhritwik9@gmail.com>
2023-03-26 14:50:44 +0530

    update

--- src/components/spec-comp/AdminSpecsComp/Admin-cocktails-detail-page/index.js
+++ src/components/spec-comp/AdminSpecsComp/Admin-cocktails-detail-page/index.js
@@ -213,8 +213,8 @@ const CocktailAdminDetailPage = ({ productId, subcategory }) => {
   function whatsthestrength(Nabv) {
     let abv = parseFloat(Nabv)
     console.log(abv);
-    if (abv > 15) return 'High'
-    if (abv > 8 && abv < 15) return 'Medium'
+    if (abv >= 15) return 'High'
+    if (abv >= 8 && abv < 15) return 'Medium'
     if (abv > 0 && abv < 8) return 'Low'
     if (abv == 0) return 'No alcohol'
     return '  '
