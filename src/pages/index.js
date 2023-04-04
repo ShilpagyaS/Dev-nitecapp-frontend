e125b06a67ab136cbcc103ed4f08a247e8bd92cf
nanak seth <millionairemindset144@gmail.com>
2023-04-04 16:24:20 +0530

    Deployment fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,11 +13,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <LayoutWithHeader>
-        <main>
-          <div className="font-bold m-auto p-7">Hello World</div>
-        </main>
-      </LayoutWithHeader>
+    
     </>
   );
 }
