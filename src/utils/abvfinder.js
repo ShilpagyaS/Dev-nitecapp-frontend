b7eff82e89c071e87d77b3d9cdbfc654ac88bb0c
nanak seth <millionairemindset144@gmail.com>
2023-04-06 12:00:09 +0530

    Chnages in Cocktail

--- src/utils/abvfinder.js
+++ src/utils/abvfinder.js
@@ -1,8 +1,8 @@
 export const whatsthestrength = (Nabv) => {
     let abv = parseFloat(Nabv)
 
-    if (abv > 15) return 'High'
-    if (abv > 8 && abv < 15) return 'Medium'
+    if (abv >= 15) return 'High'
+    if (abv >= 8 && abv < 15) return 'Medium'
     if (abv > 0 && abv < 8) return 'Low'
     if (abv == 0) return 'No alcohol'
     return ''
