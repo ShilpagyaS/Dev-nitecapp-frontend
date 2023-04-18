88af84183064bfb678c580af3a7a0fcfa1f244a1
Ritwik Singh Yadav <singhritwik9@gmail.com>
2023-04-18 20:39:04 +0530

    changes

--- src/components/guests/GuestInfo.js
+++ src/components/guests/GuestInfo.js
@@ -55,7 +55,7 @@ function GuestInfo({ guestId }) {
             Lemon in his water, Old Fashioned with 3 cherries
           </p>
         </div>
-        <DummyNotes />
+        {/* <DummyNotes /> */}
         <div className="note-text-container flex justify-between items-center bg-[#2C2C2C] w-full py-2 px-4 rounded-[5px] text-white mb-[16px]">
           <p className=" bg-transparent mr-24px">{"Note 1"}</p>
 
