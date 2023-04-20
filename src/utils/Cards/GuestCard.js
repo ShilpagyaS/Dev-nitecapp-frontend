53537fa653b7efe0bf6fb40343233fdc4a8155c5
Ritwik Singh Yadav <singhritwik9@gmail.com>
2023-04-20 12:00:48 +0530

    guests mobile view

--- src/utils/Cards/GuestCard.js
+++ src/utils/Cards/GuestCard.js
@@ -3,10 +3,10 @@ import React from 'react'
 
 function GuestCard({ image, name, onClickHandler }) {
     return (
-        <div className='cursor-pointer px-[5px] sm:w-[140px] sm:h-[175px] border border-[#3C3C3C] rounded-[5px] flex flex-col m-[10px] justify-center items-center'
+        <div className='cursor-pointer w-[140px] h-[175px] px-[5px] sm:w-[140px] sm:h-[175px] border border-[#3C3C3C] rounded-[5px] flex flex-col m-[10px] justify-center items-center'
             onClick={onClickHandler}
         >
-            <div className='relative sm:w-[93px] sm:h-[93px] rounded-full'>
+            <div className='relative w-[90px] h-[90px] sm:w-[93px] sm:h-[93px] rounded-full'>
                 <Image src={image || '/asset/User avatar default.png'}
                     alt='userImage' fill className='object-contain rounded-full' priority/>
             </div>
