1161df5336331c2ee69d6845845363b49f370749
Ritwik Singh Yadav <singhritwik9@gmail.com>
2023-07-04 01:37:26 +0530

    content type fices for contetna dn module only

--- src/components/Learn/Udemy Learn Section/LiberaryStartScrreen.js
+++ src/components/Learn/Udemy Learn Section/LiberaryStartScrreen.js
@@ -70,7 +70,7 @@ function LiberaryStartScrreen({ itemsArray }) {
             </div>
             <div className='h-full w-full grid grid-cols-7 mt-[10px]'>
                 <div className='h-full rounded-[8px] border border-[#2F2F2F] col-span-2 p-[1px] mr-[5px] bg-[#0F0F0F]'>
-                    <AccordianForPlayerSection ChapterArray={itemsArray[Counter]} onItemClicked={(content) => { currentContentFunction({ type: content.type, content: `${content.content} , ${content.type} <--TYPE` }) }} onRightClick={() => { if (Counter < itemsArray.length - 1) setCounter(prev => prev + 1) }} onLeftClick={() => { if (Counter > 0) setCounter(prev => prev - 1) }} />
+                    <AccordianForPlayerSection ChapterArray={itemsArray[Counter]} onItemClicked={(content) => { if (content.type == 'video' || content.type == 'quiz' || content.type == 'content') currentContentFunction({ type: content.type, content: `${content.content} , ${content.type} <--TYPE` }) }} onRightClick={() => { if (Counter < itemsArray.length - 1) setCounter(prev => prev + 1) }} onLeftClick={() => { if (Counter > 0) setCounter(prev => prev - 1) }} />
                 </div>
                 <div className='h-full rounded-[8px] border border-[#2F2F2F] col-span-5 p-[1px] bg-[#383838] text-white flex items-center justify-center' >
                     {currentContent.type == 'video' &&
