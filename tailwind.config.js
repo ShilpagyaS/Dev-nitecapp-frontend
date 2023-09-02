25e26b39821e5c1bcc1afd8bbbed3cba88e6fed0
Ritwik Singh Yadav <singhritwik9@gmail.com>
2023-09-02 16:29:13 +0530

    date check

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
 
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/tailwind-datepicker-react/dist/**/*.js"
   ],
   theme: {
     extend: {
