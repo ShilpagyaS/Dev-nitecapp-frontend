adca63900483f45f9f52fc09c5f6586533aabea7
Ritwik Singh Yadav <singhritwik9@gmail.com>
2023-09-13 14:26:15 +0530

    favicon

--- src/pages/[...nav]/index.js
+++ src/pages/[...nav]/index.js
@@ -117,7 +117,7 @@ export default function Category() {
         <title>NiteCapp</title>
         <meta name="description" content="NiteCapp" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/asset/nitecappfavicon.png" />
       </Head>
 
 
